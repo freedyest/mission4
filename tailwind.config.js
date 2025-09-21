1db16143549f11cb63bee024549b6ec6dcae289d
freedyest <freedyjob@gmail.com>
2025-09-21 16:12:34 +0700

    adding css config commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./src/script.js"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
