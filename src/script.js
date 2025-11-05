294213421569d24f36c77c6cfb401d4685df8536
freedyest <freedyjob@gmail.com>
2025-11-05 18:08:21 +0700

    adding time

--- src/script.js
+++ src/script.js
@@ -20,7 +20,18 @@ const options = {
 };
 currentTime.textContent =
   "Hari & Tanggal: " + now.toLocaleDateString("id-ID", options);
+const CurrentHour = document.getElementById("CurrentHour");
 
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours().toString().padStart(2, "0");
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  let seconds = now.getSeconds().toString().padStart(2, "0");
+  CurrentHour.textContent = `Jam : ${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
 // Filter nav
 const filternav = document.getElementById("filternav");
 const filterLinks = filternav.querySelectorAll("a");
